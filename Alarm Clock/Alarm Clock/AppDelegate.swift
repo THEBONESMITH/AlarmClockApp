15956fc83a1f2bd3a8fce3c7fbd18da02020256a
Conor <XS15N1@gmail.com>
2024-04-09 08:50:58 +0100

    Close button quits

--- Alarm Clock/Alarm Clock/AppDelegate.swift
+++ Alarm Clock/Alarm Clock/AppDelegate.swift
@@ -7,21 +7,28 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
     let alarmManager = AlarmManager()
 
     func applicationDidFinishLaunching(_ notification: Notification) {
-        // Add the .resizable flag to the window style
+        // Create the window with the specified style
         window = NSWindow(
             contentRect: NSRect(x: 0, y: 0, width: 800, height: 600),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView], // Notice the addition of .resizable here
+            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
             backing: .buffered, defer: false)
         window.center()
         window.setFrameAutosaveName("Main Window")
-        window.delegate = self // Make sure to set the delegate if you want to use delegate methods
+        window.delegate = self  // Set this AppDelegate as the window's delegate
         window.makeKeyAndOrderFront(nil)
 
+        // Set the content view of the window
         let contentView = ContentView(alarmManager: self.alarmManager)
         window.contentView = NSHostingView(rootView: contentView)
     }
     
+    // Called when the window's size changes
     @objc func windowDidResize(_ notification: Notification) {
         print("Window did resize to: \(window.frame.size)")
+    }
+
+    // NSWindowDelegate method that is called just before the window closes
+    func windowWillClose(_ notification: Notification) {
+        NSApplication.shared.terminate(self) // Terminate the application
     }
 }
